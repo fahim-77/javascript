87cd87efa82cc787545c15c396c116cb0bbd73ce
fahimeh zandevakili <fahimeh.zandevakili@gmail.com>
2024-08-13 19:54:13 +0330

    Add files via upload

---
+++ jspractice4/script.js
@@ -0,0 +1,37 @@
+const users= [
+    {
+        username: "marsh",
+        password: "m1122",
+        role: "sa"
+    },
+    {
+        username: "albert",
+        password: "a3344",
+        role: "ad"
+    },
+    {
+        username: "rally22",
+        password: "r5566",
+        role: "us"
+    }
+]
+function print (username , password , role) {
+    return `Username : ${username}\n Password : ${password}\n Role : ${role}`;
+}
+const count= users.length;
+const x= prompt("please enter username :");
+for( let i=0 ; i<users.length ;i++) {
+    if ( users[i].username === x)
+    {
+        const y= prompt("please enter password");
+        if ( users[i].password === y ) {
+            const title= document.querySelector("h1");
+            console.log(title);
+            title.innerText= print(x,y,users[i].role);
+            break;
+        }
+    }
+    else if (i === count - 1) {
+        alert("Username or Password is wrong");
+    }
+} 
